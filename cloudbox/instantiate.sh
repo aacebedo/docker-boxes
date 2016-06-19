d1fe3e43865d60c12af55e74e53e2899e1faf7f9
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-06-19 13:29:52 +0200

    Activated instantiation

--- cloudbox/instantiate.sh
+++ cloudbox/instantiate.sh
@@ -2,12 +2,13 @@
 
 export BASE_IMAGE="ubuntu:latest"
 export DOCKERIZE_ARCH="amd64"
-export SEAFILE_DOMAIN="acebedo.fr"
+export SEAFILE_HOSTNAME="seafile.example.com"
 export SEAFILE_SERVERNAME="seafile"
-export SEAFILE_ADMIN_EMAIL="alexandre@acebedo.fr"
+export SEAFILE_ADMIN_EMAIL="root@example.com"
 export SEAFILE_ADMIN_PASSWORD="password"
-
+export SEAFILE_URL="https://bintray.com/artifact/download/seafile-org/seafile/seafile-server_5.1.3_x86-64.tar.gz"
+export SEAFILE_PORT="80"
 export SEAFILE_DATADIR="/tmp/seafile"
 export CLOUDBOX_NETWORK="traefik_network"
 
-#dockerize -template ./seafile/Dockerfile.tmpl:./seafile/Dockerfile
+dockerize -template ./seafile/Dockerfile.tmpl:./seafile/Dockerfile
