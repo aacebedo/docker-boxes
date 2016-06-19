43b3a9434aa2c0341e7ff7c2b374ec64ad02ec14
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-06-19 16:52:59 +0200

    Removed useless args

--- cloudbox/instantiate.sh
+++ cloudbox/instantiate.sh
@@ -4,11 +4,11 @@ export BASE_IMAGE="ubuntu:latest"
 export DOCKERIZE_ARCH="amd64"
 export SEAFILE_HOSTNAME="seafile.example.com"
 export SEAFILE_SERVERNAME="seafile"
-export SEAFILE_ADMIN_EMAIL="root@example.com"
+export SEAFILE_ADMIN_EMAIL="admin@example.com"
 export SEAFILE_ADMIN_PASSWORD="password"
+#https://github.com/haiwen/seafile-rpi/releases/download/v${SEAFILE_VERSION}/seafile-server_stable_${SEAFILE_VERSION}_pi.tar.gz
 export SEAFILE_URL="https://bintray.com/artifact/download/seafile-org/seafile/seafile-server_5.1.3_x86-64.tar.gz"
-export SEAFILE_PORT="80"
 export SEAFILE_DATADIR="/tmp/seafile"
 export CLOUDBOX_NETWORK="traefik_network"
 
-dockerize -template ./seafile/Dockerfile.tmpl:./seafile/Dockerfile
+dockerize -force -template ./seafile/Dockerfile.tmpl:./seafile/Dockerfile
