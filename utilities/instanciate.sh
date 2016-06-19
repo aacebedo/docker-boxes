ee99da6bb14b68bb9baa7b1494cfd88851376011
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-06-19 13:30:25 +0200

    Replace env by arg in utilities

--- utilities/instanciate.sh
+++ utilities/instanciate.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 export BASE_IMAGE="alpine:latest"
 export DOCKERIZE_ARCH="amd64"
 export TRAEFIK_ARCH="amd64"
-export TRAEFIK_DOMAIN="domain.com"
-export TRAEFIK_SUBDOMAINS=""
-export TRAEFIK_ADMIN_EMAIL="root@domain.com"
-export TRAEFIK_DATADIR=""
-export UTILITIES_NETWORK="utilities_network"
+export TRAEFIK_DOMAIN="acebedo.fr"
+export TRAEFIK_SUBDOMAINS='"seafile.acebedo.fr"'
+export TRAEFIK_ADMIN_EMAIL="alexandre@acebedo.fr"
+export TRAEFIK_DATADIR="/tmp/traefik"
+export TRAEFIK_PORT="80"
+export TRAEFIK_SECUREPORT="443"
+export TRAEFIK_CONTROLPORT="82"
+export UTILITIES_NETWORK="traefik_network"
 
 dockerize -force -template ./traefik/Dockerfile.tmpl:./traefik/Dockerfile
