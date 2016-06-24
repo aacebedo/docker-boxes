91af6fc747268eb7b13f039351e057aeb6065e9f
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-06-24 21:03:55 +0200

    Replaced URL variables

--- utilities/instanciate.sh
+++ utilities/instanciate.sh
@@ -10,6 +10,11 @@ export TRAEFIK_DATADIR="/tmp/traefik"
 export TRAEFIK_PORT="80"
 export TRAEFIK_SECUREPORT="443"
 export TRAEFIK_CONTROLPORT="82"
+export CONSUL_PORT1="8400"
+export CONSUL_PORT2="8500"
+export CONSUL_PORT3="53"
+
 export UTILITIES_NETWORK="traefik_network"
 
 dockerize -force -template ./traefik/Dockerfile.tmpl:./traefik/Dockerfile
+dockerize -force -template ./consul/Dockerfile.tmpl:./consul/Dockerfile
