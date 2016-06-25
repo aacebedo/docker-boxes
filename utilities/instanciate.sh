f93aa07ca5011778f67b6b2b8972a620522cdd37
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-06-25 10:00:39 +0200

    Bumped CFDNSUpdater version

--- utilities/instanciate.sh
+++ utilities/instanciate.sh
@@ -10,11 +10,15 @@ export TRAEFIK_DATADIR="/tmp/traefik"
 export TRAEFIK_PORT="80"
 export TRAEFIK_SECUREPORT="443"
 export TRAEFIK_CONTROLPORT="82"
-export CONSUL_PORT1="8400"
-export CONSUL_PORT2="8500"
-export CONSUL_PORT3="53"
+export CFDNSUPDATER_DOMAIN="example.com"
+export CFDNSUPDATER_EMAIL="admin@example.com"
+export CFDNSUPDATER_APIKEY="none"
+export CFDNSUPDATER_PERIOD="60"
+export CFDNSUPDATER_RECORDTYPES=""
+export CFDNSUPDATER_RECORDNAMES=""
+export CFDNSUPDATER_ARCH="amd64"
 
 export UTILITIES_NETWORK="traefik_network"
 
 dockerize -force -template ./traefik/Dockerfile.tmpl:./traefik/Dockerfile
-dockerize -force -template ./consul/Dockerfile.tmpl:./consul/Dockerfile
+dockerize -force -template ./cfdnsupdater/Dockerfile.tmpl:./cfdnsupdater/Dockerfile
