c97f66db742d550c57410863bc0468be83b33ae4
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-06-26 10:23:08 +0200

    Added version

--- blogbox/instantiate.sh
+++ blogbox/instantiate.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
 export BASE_IMAGE="alpine:latest"
+
 export DOCKERIZE_ARCH="amd64"
-export DOMAIN="example.com"
+
+export GHOST_VERSION="0.8.0"
+export GHOST_HOSTNAME="blog.example.com"
 export GHOST_DATADIR="/tmp/ghost"
+
 export BLOGBOX_NETWORK="traefik_network"
 
 dockerize -force -template ./ghost/Dockerfile.tmpl:./ghost/Dockerfile
