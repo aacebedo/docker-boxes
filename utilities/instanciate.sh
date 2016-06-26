c6c0a693f40f10f8970761196d542f71ca40eb27
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-06-26 10:15:06 +0200

    Added version support

--- utilities/instanciate.sh
+++ utilities/instanciate.sh
@@ -2,7 +2,9 @@
 
 export BASE_IMAGE="alpine:latest"
 export DOCKERIZE_ARCH="amd64"
+
 export TRAEFIK_ARCH="amd64"
+export TRAEFIK_VERSION="1.0.0-rc2"
 export TRAEFIK_DOMAIN="acebedo.fr"
 export TRAEFIK_SUBDOMAINS='"seafile.acebedo.fr"'
 export TRAEFIK_ADMIN_EMAIL="alexandre@acebedo.fr"
@@ -10,6 +12,7 @@ export TRAEFIK_DATADIR="/tmp/traefik"
 export TRAEFIK_PORT="80"
 export TRAEFIK_SECUREPORT="443"
 export TRAEFIK_CONTROLPORT="82"
+
 export CFDNSUPDATER_DOMAIN="example.com"
 export CFDNSUPDATER_EMAIL="admin@example.com"
 export CFDNSUPDATER_APIKEY="none"
@@ -17,6 +20,7 @@ export CFDNSUPDATER_PERIOD="60"
 export CFDNSUPDATER_RECORDTYPES=""
 export CFDNSUPDATER_RECORDNAMES=""
 export CFDNSUPDATER_ARCH="amd64"
+export CFDNSUPDATER_VERSION="0.9.2"
 
 export UTILITIES_NETWORK="traefik_network"
 
