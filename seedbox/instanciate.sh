1b8ab21abd88d328e0a2e8c91367d2daa1504eb0
Alexandre ACEBEDO <alexandre@acebedo.fr>
2016-06-26 10:14:36 +0200

    Added version support

--- seedbox/instanciate.sh
+++ seedbox/instanciate.sh
@@ -2,6 +2,11 @@
 
 export BASE_IMAGE="alpine:latest"
 export DOCKERIZE_ARCH="amd64"
+
+export COUCHPOTATO_VERSION="3.0.1"
+export SICKRAGE_VERSION="2016.05.20-1"
+export NZBGET_VERSION="16.4"
+
 export NZBSDIR="/tmp/medias"
 export MOVIESDIR="/tmp/medias"
 export TORRENTSDIR="/tmp/medias"
