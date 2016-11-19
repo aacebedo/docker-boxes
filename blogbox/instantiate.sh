76c47cb18df47631491001801ad36d160be1ee55
Alexandre ACEBEDO <alexandre@cebedo.fr>
2016-11-19 15:14:00 +0100

    Refactored ghost
    Added Rockerfile
    Added supervisor

--- blogbox/instantiate.sh
+++ blogbox/instantiate.sh
@@ -2,12 +2,11 @@
 
 export BASE_IMAGE="alpine:latest"
 
-export DOCKERIZE_ARCH="amd64"
-
-export GHOST_VERSION="0.8.0"
-export GHOST_HOSTNAME="blog.example.com"
-export GHOST_DATADIR="/tmp/ghost"
+export ARCH="amd64"
+export GHOST_VERSION="0.11.3"
+export GHOST_HOSTNAME="http://blog.example.com"
+export GHOST_DATADIR="/mnt/data/ghost"
 
 export BLOGBOX_NETWORK="traefik_network"
 
-dockerize -force -template ./ghost/Dockerfile.tmpl:./ghost/Dockerfile
+rocker build -var ARCH=${ARCH} -var GHOST_VERSION=${GHOST_VERSION} -var IS_LATEST=true -f ./ghost/Rockerfile ./ghost
