1fa941f7a1abd2ba6ed9004387eaf372b163814a
Alexandre ACEBEDO <alexandre@acebedo.fr>
2017-02-18 14:13:17 +0100

    Updated version and added calls to rocker

--- utilities/instantiate.sh
+++ utilities/instantiate.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-export BASE_IMAGE="alpine:latest"
 export ARCH="amd64"
 
-export TRAEFIK_VERSION="1.1.0"
+export TRAEFIK_VERSION="1.2.0-rc1"
 export TRAEFIK_DOMAIN="example.com"
 export TRAEFIK_SUBDOMAINS='"subdomain.example.com"'
 export TRAEFIK_ADMIN_EMAIL="admin@example.com"
@@ -21,11 +20,22 @@ export CFDNSUPDATER_RECORDTYPES=""
 export CFDNSUPDATER_RECORDNAMES=""
 export CFDNSUPDATER_VERSION="0.9.2"
 
-export DNSDOCK_VERSION="1.12.1"
+export DNSDOCK_VERSION="1.16.1"
 export DNSDOCK_DOMAIN="example.com"
 export DNSDOCK_PORT="53"
 export DNSDOCK_CONTROLPORT="83"
 
 export UTILITIES_NETWORK="traefik_network"
 
-rocker build -var ARCH=amd64 -var TRAEFIK_VERSION=${TRAEFIK_VERSION} -var IS_LATEST=true -f ./traefik/Rockerfile ./traefik
+rocker build -var ARCH=amd64 \
+             -var TRAEFIK_VERSION=${TRAEFIK_VERSION} \
+             -var IS_LATEST=true \
+             -f ./traefik/Rockerfile ./traefik
+rocker build -var ARCH=amd64 \
+             -var DNSDOCK_VERSION=${DNSDOCK_VERSION} \
+             -var IS_LATEST=true \
+             -f ./dnsdock/Rockerfile ./dnsdock
+rocker build -var ARCH=amd64 \
+             -var CFDNSUPDATER_VERSION=${CFDNSUPDATER_VERSION} \
+             -var IS_LATEST=true \
+             -f ./cfdnsupdater/Rockerfile ./cfdnsupdater
