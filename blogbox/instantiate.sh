0490c7939106a74a1f12fe209fcf6e61684d651a
Alexandre ACEBEDO <alexandre@acebedo.fr>
2017-02-18 14:14:44 +0100

    Updated version

--- blogbox/instantiate.sh
+++ blogbox/instantiate.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
-export BASE_IMAGE="alpine:latest"
-
 export ARCH="amd64"
-export GHOST_VERSION="0.11.3"
+export GHOST_VERSION="1.0.0-alpha.12"
 export GHOST_HOSTNAME="http://blog.example.com"
 export GHOST_DATADIR="/mnt/data/ghost"
 
 export BLOGBOX_NETWORK="traefik_network"
 
-rocker build -var ARCH=${ARCH} -var GHOST_VERSION=${GHOST_VERSION} -var IS_LATEST=true -f ./ghost/Rockerfile ./ghost
+rocker build -var ARCH=${ARCH} \
+             -var GHOST_VERSION=${GHOST_VERSION} \
+             -var IS_LATEST=true \
+             -f ./ghost/Rockerfile ./ghost
