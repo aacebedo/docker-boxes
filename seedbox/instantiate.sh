069511d73269f173ebb697a3a060d9fae55084aa
Alexandre ACEBEDO <alexandre@acebedo.fr>
2017-02-18 14:19:59 +0100

    Updated version

--- seedbox/instantiate.sh
+++ seedbox/instantiate.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
-export BASE_IMAGE="alpine:latest"
-export DOCKERIZE_ARCH="amd64"
+export ARCH="amd64"
 
 export COUCHPOTATO_VERSION="3.0.1"
-export SICKRAGE_VERSION="2016.05.20-1"
+export SICKRAGE_VERSION="2017.02.15-1"
 export NZBGET_VERSION="16.4"
 
 export NZBSDIR="/tmp/medias"
@@ -14,7 +13,17 @@ export TVSHOWSDIR="/tmp/medias"
 export DOMAIN="example.com"
 export SEEDBOX_NETWORK="traefik_network"
 
-dockerize -force -template ./nzbget/Dockerfile.tmpl:./nzbget/Dockerfile
-dockerize -force -template ./sickrage/Dockerfile.tmpl:./sickrage/Dockerfile
-dockerize -force -template ./transmission/Dockerfile.tmpl:./transmission/Dockerfile
-dockerize -force -template ./couchpotato/Dockerfile.tmpl:./couchpotato/Dockerfile
+rocker build -var ARCH=amd64 \
+           -var COUCHPOTATO_VERSION=${COUCHPOTATO_VERSION} \
+           -var IS_LATEST=true \
+           -f ./couchpotato/Rockerfile ./couchpotato
+rocker build -var ARCH=amd64 \
+           -var NZBGET_VERSION=${NZBGET_VERSION} \
+           -var IS_LATEST=true \
+           -f ./nzbget/Rockerfile ./nzbget
+rocker build -var ARCH=amd64 \
+           -var SICKRAGE_VERSION=${SICKRAGE_VERSION} \
+            -var IS_LATEST=true \
+            -f ./sickrage/Rockerfile ./sickrage
+rocker build -var ARCH=amd64 \
+             -f ./transmission/Rockerfile ./transmission
