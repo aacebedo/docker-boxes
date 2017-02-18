fd88c50959cf9e66d40663c2cb853b122f16d67d
Alexandre ACEBEDO <alexandre@acebedo.fr>
2017-02-18 14:23:37 +0100

    Updated version

--- cloudbox/instantiate.sh
+++ cloudbox/instantiate.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 
-export BASE_IMAGE="ubuntu:latest"
-
-export DOCKERIZE_ARCH="amd64"
+export ARCH="amd64"
 
 export SEAFILE_VERSION="5.1.3"
 export SEAFILE_HOSTNAME="seafile.example.com"
@@ -10,7 +8,6 @@ export SEAFILE_SERVERNAME="seafile"
 export SEAFILE_ADMIN_EMAIL="admin@example.com"
 export SEAFILE_ADMIN_PASSWORD="password"
 export SEAFILE_PROTOCOL="https"
-#export SEAFILE_URL="https://github.com/haiwen/seafile-rpi/releases/download/v${SEAFILE_VERSION}/seafile-server_stable_${SEAFILE_VERSION}_pi.tar.gz"
 export SEAFILE_URL="https://bintray.com/artifact/download/seafile-org/seafile/seafile-server_${SEAFILE_VERSION}_x86-64.tar.gz"
 export SEAFILE_DATADIR="/tmp/seafile/data"
 export SEAFILE_CONFDIR="/tmp/seafile/conf"
@@ -19,6 +16,18 @@ export SEAFILE_SMTP_SERVER="smtp.example.com"
 export SEAFILE_SMTP_EMAIL="admin@example"
 export SEAFILE_SMTP_PASSWORD="password"
 
+export ROUNDCUBE_VERSION="1.3-beta"
+export ROUNDCUBE_SMTP_SERVER="ssl://smtp.example.com"
+export ROUNDCUBE_IMAP_SERVER="ssl://imap.example.com"
+export ROUNDCUBE_DES_KEY="aaaaaaaaaaaaaaaaaaaaaaaa"
+
 export CLOUDBOX_NETWORK="traefik_network"
 
-dockerize -force -template ./seafile/Dockerfile.tmpl:./seafile/Dockerfile
+rocker build -var ARCH=amd64 \
+             -var SEAFILE_VERSION=${SEAFILE_VERSION} \
+             -var IS_LATEST=true \
+             -f ./seafile/Rockerfile ./seafile
+rocker build -var ARCH=amd64 \
+             -var ROUNDCUBE_VERSION=${ROUNDCUBE_VERSION} \
+             -var IS_LATEST=true \
+             -f ./roundcube/Rockerfile ./roundcube
